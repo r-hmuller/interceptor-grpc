475db3606a1888b75aadffd5acec57ba4e139223
Rodrigo Müller <contato@rodrigo.muller.nom.br>
2024-09-09 21:19:47 +0100

    Improve performance and add new debugging techniques

--- main.go
+++ main.go
@@ -1,21 +1,48 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"github.com/gorilla/mux"
 	"interceptor-grpc/config"
 	"interceptor-grpc/interceptor"
 	"log"
 	"net/http"
+	"os"
+	"runtime/trace"
 	"sync"
+	"time"
 )
 
 func main() {
+	if config.GetEnableTrace() {
+		go func() {
+			f, _ := os.Create("trace.out")
+			trace.Start(f)
+			start := time.Now()
+			duration := 153 * time.Second
+
+			for time.Since(start) < duration {
+				// Your code here
+				time.Sleep(1 * time.Second) // Adjust the sleep duration as needed
+			}
+			trace.Stop()
+		}()
+	}
+
 	config.VerifyEnvVars()
 
+	ctx := context.Background()
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go startListener()
+	wg.Add(1)
+	go func() {
+		err := config.SetRequestToNewStatus(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
 	wg.Wait()
 }
