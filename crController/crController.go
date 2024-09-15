579f3fc56785c7898938736a9e60331e6aa98be5
Rodrigo Muller <r.muller@global.leaseweb.com>
2024-09-15 17:13:50 +0100

    Add grpc

--- crController/crController.go
+++ crController/crController.go
@@ -1,11 +1,48 @@
 package crController
 
-import "sync/atomic"
+import (
+	"context"
+	"google.golang.org/grpc"
+	crController "interceptor-grpc/protos"
+	"log"
+	"net"
+	"sync/atomic"
+)
 
 var IsRunningPendingRequestQueue atomic.Bool
 var IsDoingSnapshot atomic.Bool
 var IsRestoringSnapshot atomic.Bool
+var IsContainerUnavailable atomic.Bool
+
+type server struct {
+	crController.UnimplementedFailureServiceServer
+}
+
+func (s *server) StopRequests(_ context.Context, _ *crController.RestoreRequest) (*crController.RestoreResponse, error) {
+	IsContainerUnavailable.Store(true)
+	return &crController.RestoreResponse{Message: true}, nil
+}
+
+func (s *server) ReprocessRequests(_ context.Context, _ *crController.RestoreRequest) (*crController.RestoreResponse, error) {
+	IsContainerUnavailable.Store(false)
+
+	return &crController.RestoreResponse{Message: true}, nil
+}
 
 func IsUnavailable() bool {
-	return IsRunningPendingRequestQueue.Load() || IsDoingSnapshot.Load() || IsRestoringSnapshot.Load()
+	return IsRunningPendingRequestQueue.Load() || IsDoingSnapshot.Load() || IsRestoringSnapshot.Load() || IsContainerUnavailable.Load()
+}
+
+func RunGRPCServer() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen on port 50051: %v", err)
+	}
+
+	s := grpc.NewServer()
+	crController.RegisterFailureServiceServer(s, &server{})
+	log.Printf("gRPC server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module interceptor-grpc
 go 1.22
 
 require (
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/rs/zerolog v1.33.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 )
